1611c1a8b8ef29882db78a30e23f786ae7a3ee85
CORNELIUSTM23 <CorneliusThomas23@gcc.edu>
2024-02-19 15:08:15 -0500

    Added Corn

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
-        System.out.println("updat");
+        System.out.println("update");
+        System.out.println("Corn");
     }
 }
